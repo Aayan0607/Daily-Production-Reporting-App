a8a671988d848777ee62cff337b783ad4442d635
Aayan Tamboli <tamboliaayan4225@gmail.com>
2026-06-28 12:34:02 +0530

    Base app setup for expo

---
+++ production-reporting-system/App.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Daily Production Reporting System</Text>
+    </View>
+  );
+}
